b4455c4b953bdfa0771b5c29a9dd8a432daa2f47
Emily Fortuna <efortuna@google.com>
2018-12-28 16:45:37 -0800

    currently just browses adopt-a-pet with a hard-coded starting page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+    home: FullDogView(
+        initialUrl:
+            'https://www.adoptapet.com/pet/23665138-seattle-washington-german-shepherd-dog')));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class FullDogView extends StatefulWidget {
+  FullDogView({this.initialUrl});
+  final String initialUrl;
+  @override
+  _FullDogViewState createState() => _FullDogViewState();
+}
+
+class _FullDogViewState extends State<FullDogView> {
+  Completer<WebViewController> _controller = Completer<WebViewController>();
+  final Set<String> _favorites = Set<String>();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dog Stats'),
+        actions: <Widget>[
+          // TODO(efortuna): make this not a dropdown and just an icon?
+          Menu(_controller.future, () => _favorites),
+        ],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: WebView(
+        initialUrl: widget.initialUrl,
+        javaScriptMode: JavaScriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+      floatingActionButton: _bookmarkButton(),
+    );
+  }
+
+  _bookmarkButton() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return FloatingActionButton(
+            onPressed: () async {
+              var url = await controller.data.currentUrl();
+              _favorites.add(url);
+              Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text('Saved $url for later reading.')),
+              );
+            },
+            child: Icon(Icons.favorite),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Menu extends StatelessWidget {
+  Menu(this._webViewControllerFuture, this.favoritesAccessor);
+  final Future<WebViewController> _webViewControllerFuture;
+  // TODO(efortuna): Come up with a more elegant solution for an accessor to this than a callback.
+  final Function favoritesAccessor;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return FutureBuilder(
+      future: _webViewControllerFuture,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (!controller.hasData) return Container();
+        return PopupMenuButton<String>(
+          onSelected: (String value) async {
+            if (value == 'Email link') {
+              var url = await controller.data.currentUrl();
+              await launch(
+                  'mailto:?subject=Check out this cool Wikipedia page&body=$url');
+            } else {
+              var newUrl = await Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return FavoritesPage(favoritesAccessor());
+              }));
+              Scaffold.of(context).removeCurrentSnackBar();
+              if (newUrl != null) controller.data.loadUrl(newUrl);
+            }
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuItem<String>>[
+                const PopupMenuItem<String>(
+                  value: 'Email link',
+                  child: Text('Email link'),
+                ),
+                const PopupMenuItem<String>(
+                  value: 'See Favorites',
+                  child: Text('See Favorites'),
+                ),
+              ],
+        );
+      },
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  FavoritesPage(this.favorites);
+  final Set<String> favorites;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text('Favorite pages')),
+      body: ListView(
+          children: favorites
+              .map((url) => ListTile(
+                  title: Text(url), onTap: () => Navigator.pop(context, url)))
+              .toList()),
     );
   }
 }
