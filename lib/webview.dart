863a759d3346bfd8b6f8676c5fd420505e9aadc5
Emily Fortuna <efortuna@google.com>
2019-01-14 08:09:13 -0800

    switch to builder instead of SimpleFutureBuilder

--- lib/webview.dart
+++ lib/webview.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:the_doghouse/data/data.dart';
-import 'package:simple_future_builder/simple_future_builder.dart';
 import 'package:the_doghouse/data/model.dart';
 import 'package:the_doghouse/main.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -27,9 +26,8 @@ class FullDogView extends StatelessWidget {
   }
 
   _bookmarkButton(BuildContext context) {
-    return SimpleFutureBuilder<WebViewController>(
-      future: _controller.future,
-      builder: (BuildContext context, _) {
+    return Builder(
+      builder: (BuildContext context) {
         return FloatingActionButton(
           child: const Icon(Icons.favorite),
           onPressed: () async {
